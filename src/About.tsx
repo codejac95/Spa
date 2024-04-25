d62ce4eb95a7990a1269de749eea74a105a0fe5e
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-04-25 11:58:09 +0200

    build

--- src/About.tsx
+++ src/About.tsx
@@ -3,13 +3,17 @@ import './App.css'
 function About() {
 
   return (
-   
-    <div>
+   <><div>
       <h2>Välkommen till oss på Jacob Spa & Vin</h2>
       <h3>Vi lever för spavistelse och goda viner</h3>
       <h3>Öppetider:</h3>
       <h4>Alla dagar förutom måndagar samt helgdagar</h4>
     </div>
+    <div className='wineAndSpa'>
+<br />
+    </div>
+   </>
+    
   );
 }
 
--- src/Booking.tsx
+++ src/Booking.tsx
@@ -1,38 +1,151 @@
+// import { useEffect, useState } from 'react';
+// import Calendar from 'react-calendar';
+// import './App.css'
+
+// type TimeSlot = 'Morgon' | 'Eftermiddag' | 'Kväll';
+
+// interface BookingInfo  {
+//   date: string;
+//   timeSlot: TimeSlot;
+// };
+
+// function Booking({warmOrCold}: {warmOrCold: 'warm' | 'cold'}) {
+//   const [selectedDate, setSelectedDate] = useState<Date>()
+//   const [bookedSlots, setBookedSlots] = useState<BookingInfo[]>([]);
+//   const [holidays, setHolidays] = useState<string[]>([])
+
+//   useEffect(() => {
+//     fetchRedDays();
+//   },[])
+
+//   const fetchRedDays = () => {
+//     fetch('http://sholiday.faboul.se/dagar/v2.1/2024')
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         const holidayArray = data.dagar.filter((day: any) => day.hasOwnProperty("helgdag"))
+//                                         .map((day: any) => day.datum);
+//         setHolidays(holidayArray);
+//       })
+//   };
+
+//   const isMonday =(checkDate: Date) => {
+//     return checkDate.getDay()=== 1;
+//   }
+
+//   const isHoliday = (checkDate: Date) => {
+//     const checkMonth = checkDate.getMonth() + 1;
+//     const checkDay = checkDate.getDate();
+//     return holidays.some((holiday: string) => {
+//       const [holidayMonth, holidayDay] = holiday.split('-').slice(1).map(Number);
+//       return checkMonth === holidayMonth && checkDay === holidayDay;
+//     });
+//   };
+
+//   const localStorageKey = warmOrCold === 'warm' ? 'warmBookings' : 'coldBookings';
+
+//   const handleDateSelect = (date: Date) => {
+//     setSelectedDate(date);
+//     const storedBookings = JSON.parse(localStorage.getItem(localStorageKey) ?? '[]') as BookingInfo[];
+//     const selectedBookings = storedBookings.filter(booking => booking.date === date.toLocaleDateString().slice(0, 10));
+//     setBookedSlots(selectedBookings);
+//   };
+
+
+//   const isSlotBooked = (timeSlot: TimeSlot) => {
+//     return bookedSlots.some(booking => booking.timeSlot === timeSlot);
+//   };
+
+//   const bookSlot = (timeSlot: TimeSlot) => {
+//     if (selectedDate && !holidays.includes(selectedDate.toLocaleDateString().slice(0,10))) {
+//       const bookingInfo: BookingInfo = {
+//         date: selectedDate.toLocaleDateString().slice(0, 10),
+//         timeSlot: timeSlot,
+//       };
+//       const bookings = JSON.parse(localStorage.getItem(localStorageKey) ?? '[]') as BookingInfo[];
+//       localStorage.setItem(localStorageKey, JSON.stringify([...bookings, bookingInfo]));
+//       setBookedSlots([...bookedSlots, bookingInfo]);
+//     }
+//   };
+
+//   const unbookSlot = (timeSlot: TimeSlot) => {
+//     if (selectedDate) {
+//       const updatedBookings = bookedSlots.filter(booking => !(booking.date === selectedDate.toLocaleDateString().slice(0, 10) && booking.timeSlot === timeSlot));
+//       setBookedSlots(updatedBookings);
+//       localStorage.setItem(localStorageKey, JSON.stringify(updatedBookings));
+//     }
+//   };
+  
+//   const handleBookingButton = (timeSlot: TimeSlot) => {
+//     if (isSlotBooked(timeSlot)) {
+//       unbookSlot(timeSlot);
+//     } else {
+//       bookSlot(timeSlot);
+//     }
+//   };
+
+//   return (
+//     <div className="booking">
+//       <Calendar onClickDay={handleDateSelect}/>
+//       {selectedDate && 
+//         <div className="selectedDate">
+//           <p>Datum: {selectedDate.toLocaleDateString()}</p>
+  
+//           <p>Förmiddag: <span className={isSlotBooked('Morgon') ? 'redText' : 'greenText'}>{isSlotBooked('Morgon') ? 'Bokad' : 'Ledig'}</span>
+//           <button onClick={() => handleBookingButton('Morgon')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+//             {isSlotBooked('Morgon') ? 'Avboka förmiddag' : 'Boka förmiddag'}
+//           </button></p>
+  
+//           <p>Eftermiddag: <span className={isSlotBooked('Eftermiddag') ? 'redText' : 'greenText'}>{isSlotBooked('Eftermiddag') ? 'Bokad' : 'Ledig'}</span>
+//           <button onClick={() => handleBookingButton('Eftermiddag')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+//             {isSlotBooked('Eftermiddag') ? 'Avboka eftermiddag' : 'Boka eftermiddag'}
+//           </button></p>
+          
+//           <p>Kväll: <span className={isSlotBooked('Kväll') ? 'redText' : 'greenText'}>{isSlotBooked('Kväll') ? 'Bokad' : 'Ledig'}</span>
+//           <button onClick={() => handleBookingButton('Kväll')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+//             {isSlotBooked('Kväll') ? 'Avboka kväll' : 'Boka kväll'}
+//           </button></p>
+//         </div>
+//       }
+//     </div>
+//   );
+// }
+// export default Booking;
+
 import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
-import './App.css'
+import './App.css';
 
 type TimeSlot = 'Morgon' | 'Eftermiddag' | 'Kväll';
 
-interface BookingInfo  {
+interface BookingInfo {
   date: string;
   timeSlot: TimeSlot;
-};
+}
 
-function Booking({warmOrCold}: {warmOrCold: 'warm' | 'cold'}) {
-  const [selectedDate, setSelectedDate] = useState<Date>()
+function Booking({ warmOrCold }: { warmOrCold: 'warm' | 'cold' }) {
+  const [selectedDate, setSelectedDate] = useState<Date>();
   const [bookedSlots, setBookedSlots] = useState<BookingInfo[]>([]);
-  const [holidays, setHolidays] = useState<string[]>([])
+  const [holidays, setHolidays] = useState<string[]>([]);
 
   useEffect(() => {
     fetchRedDays();
-  },[])
+  }, []);
 
   const fetchRedDays = () => {
     fetch('http://sholiday.faboul.se/dagar/v2.1/2024')
-      .then(response => {
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
         const holidayArray = data.dagar.filter((day: any) => day.hasOwnProperty("helgdag"))
-                                        .map((day: any) => day.datum);
+          .map((day: any) => day.datum);
         setHolidays(holidayArray);
-      })
+      });
   };
 
-  const isMonday =(checkDate: Date) => {
-    return checkDate.getDay()=== 1;
-  }
+  const isMonday = (checkDate: Date) => {
+    return checkDate.getDay() === 1;
+  };
 
   const isHoliday = (checkDate: Date) => {
     const checkMonth = checkDate.getMonth() + 1;
@@ -43,13 +156,12 @@ function Booking({warmOrCold}: {warmOrCold: 'warm' | 'cold'}) {
     });
   };
 
-  const localStorageKey = warmOrCold === 'warm' ? 'warmBookings' : 'coldBookings';
-
   const handleDateSelect = (date: Date) => {
     setSelectedDate(date);
-    const storedBookings = JSON.parse(localStorage.getItem(localStorageKey) ?? '[]') as BookingInfo[];
-    const selectedBookings = storedBookings.filter(booking => booking.date === date.toLocaleDateString().slice(0, 10));
-    setBookedSlots(selectedBookings);
+    const selectedDateString = date.toLocaleDateString().slice(0, 10);
+    fetch(`http://localhost:8080/${warmOrCold}booking/${selectedDateString}`)
+      .then(response => response.json())
+      .then(data => setBookedSlots(data));
   };
 
   const isSlotBooked = (timeSlot: TimeSlot) => {
@@ -57,25 +169,41 @@ function Booking({warmOrCold}: {warmOrCold: 'warm' | 'cold'}) {
   };
 
   const bookSlot = (timeSlot: TimeSlot) => {
-    if (selectedDate && !holidays.includes(selectedDate.toLocaleDateString().slice(0,10))) {
+    if (selectedDate && !holidays.includes(selectedDate.toLocaleDateString().slice(0, 10))) {
       const bookingInfo: BookingInfo = {
         date: selectedDate.toLocaleDateString().slice(0, 10),
         timeSlot: timeSlot,
       };
-      const bookings = JSON.parse(localStorage.getItem(localStorageKey) ?? '[]') as BookingInfo[];
-      localStorage.setItem(localStorageKey, JSON.stringify([...bookings, bookingInfo]));
-      setBookedSlots([...bookedSlots, bookingInfo]);
+      fetch(`http://localhost:8080/${warmOrCold}booking/${selectedDate.toLocaleDateString().slice(0, 10)}/${timeSlot}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bookingInfo)
+      })
+        .then(response => response.json())
+        .then(data => {
+          setBookedSlots([...bookedSlots, data]);
+        });
     }
   };
 
   const unbookSlot = (timeSlot: TimeSlot) => {
     if (selectedDate) {
-      const updatedBookings = bookedSlots.filter(booking => !(booking.date === selectedDate.toLocaleDateString().slice(0, 10) && booking.timeSlot === timeSlot));
-      setBookedSlots(updatedBookings);
-      localStorage.setItem(localStorageKey, JSON.stringify(updatedBookings));
+      fetch(`http://localhost:8080/${warmOrCold}booking/${selectedDate.toLocaleDateString().slice(0, 10)}/${timeSlot}`, {
+        method: 'DELETE'
+      })
+        .then(response => {
+          if (response.ok) {
+            const updatedBookings = bookedSlots.filter(booking => !(booking.date === selectedDate.toLocaleDateString().slice(0, 10) && booking.timeSlot === timeSlot));
+            setBookedSlots(updatedBookings);
+          } else {
+            console.error('Failed to unbook slot');
+          }
+        });
     }
   };
-  
+
   const handleBookingButton = (timeSlot: TimeSlot) => {
     if (isSlotBooked(timeSlot)) {
       unbookSlot(timeSlot);
@@ -86,25 +214,25 @@ function Booking({warmOrCold}: {warmOrCold: 'warm' | 'cold'}) {
 
   return (
     <div className="booking">
-      <Calendar onClickDay={handleDateSelect}/>
-      {selectedDate && 
+      <Calendar onClickDay={handleDateSelect} />
+      {selectedDate &&
         <div className="selectedDate">
           <p>Datum: {selectedDate.toLocaleDateString()}</p>
-  
+
           <p>Förmiddag: <span className={isSlotBooked('Morgon') ? 'redText' : 'greenText'}>{isSlotBooked('Morgon') ? 'Bokad' : 'Ledig'}</span>
-          <button onClick={() => handleBookingButton('Morgon')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
-            {isSlotBooked('Morgon') ? 'Avboka förmiddag' : 'Boka förmiddag'}
-          </button></p>
-  
+            <button onClick={() => handleBookingButton('Morgon')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+              {isSlotBooked('Morgon') ? 'Avboka morgon' : 'Boka morgon'}
+            </button></p>
+
           <p>Eftermiddag: <span className={isSlotBooked('Eftermiddag') ? 'redText' : 'greenText'}>{isSlotBooked('Eftermiddag') ? 'Bokad' : 'Ledig'}</span>
-          <button onClick={() => handleBookingButton('Eftermiddag')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
-            {isSlotBooked('Eftermiddag') ? 'Avboka eftermiddag' : 'Boka eftermiddag'}
-          </button></p>
-          
+            <button onClick={() => handleBookingButton('Eftermiddag')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+              {isSlotBooked('Eftermiddag') ? 'Avboka eftermiddag' : 'Boka eftermiddag'}
+            </button></p>
+
           <p>Kväll: <span className={isSlotBooked('Kväll') ? 'redText' : 'greenText'}>{isSlotBooked('Kväll') ? 'Bokad' : 'Ledig'}</span>
-          <button onClick={() => handleBookingButton('Kväll')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
-            {isSlotBooked('Kväll') ? 'Avboka kväll' : 'Boka kväll'}
-          </button></p>
+            <button onClick={() => handleBookingButton('Kväll')} disabled={isHoliday(selectedDate) || isMonday(selectedDate)}>
+              {isSlotBooked('Kväll') ? 'Avboka kväll' : 'Boka kväll'}
+            </button></p>
         </div>
       }
     </div>
